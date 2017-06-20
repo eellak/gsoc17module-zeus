ac9fcf6657f5f106ce63787487bd2c8921b06bf2
Vitalis Salis <vitsalis@gmail.com>
2017-06-20 13:02:52 +0300

    Add wrapper for G1 group and addition on G2 group

--- demolibff/g2_wrapper_py.pyx
+++ demolibff/g2_wrapper_py.pyx
@@ -1,8 +1,8 @@
 import datetime
 cimport g2_wrapper_py
 
-cpdef mul(int n):
-    cdef G2Elem *g2 = new G2Elem(), *res
+def demoG2(int n):
+    cdef G2Elem *g2 = new G2Elem(), *other = new G2Elem(), *res
     cdef BigNum* bg = new BigNum()
 
     g2[0].init(n)
@@ -16,4 +16,39 @@ cpdef mul(int n):
 
     end = datetime.datetime.now()
 
-    return end - start
+    print ("multiplication - G2: %s" % (end - start))
+
+    start = datetime.datetime.now()
+
+    for i in range(n):
+        res = g2[0].add(other)
+
+    end = datetime.datetime.now()
+
+    print ("addition - G2: %s" % (end - start))
+
+def demoG1(int n):
+    cdef G1Elem *g1 = new G1Elem(), *other = new G1Elem(), *res
+    cdef BigNum* bg = new BigNum()
+
+    g1[0].init(n)
+
+    cdef Fr[curve] elem = bg[0].get_elem()
+
+    start = datetime.datetime.now()
+
+    for i in range(n):
+        res = g1[0].mul(elem)
+
+    end = datetime.datetime.now()
+
+    print ("multiplication - G1: %s" % (end - start))
+
+    start = datetime.datetime.now()
+
+    for i in range(n):
+        res = g1[0].add(other)
+
+    end = datetime.datetime.now()
+
+    print ("addition - G1: %s" % (end - start))
