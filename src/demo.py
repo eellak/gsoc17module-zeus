6b5619987e7adc4b840657ebe118c1b7a94cdb2f
Vitalis Salis <vitsalis@gmail.com>
2017-07-17 13:33:41 +0300

    Add a demo containing the whole process

---
+++ src/demo.py
@@ -0,0 +1,27 @@
+import datetime, sys
+
+from crs import CRS
+from prover import Prover
+from verifier import Verifier
+from utils import make_s_randoms, random_permutation,\
+        encrypt_messages, make_tables, decrypt_messages
+
+n = int(sys.argv[1])
+
+start = datetime.datetime.now()
+
+crs = CRS(n)
+s_randoms = make_s_randoms(n, crs.order)
+sigma = random_permutation(n)
+ciphertexts = encrypt_messages(crs.order, crs.pk1, crs.pk2, list(range(n)))
+prover = Prover(crs)
+proof = prover.prove(n, ciphertexts, sigma, s_randoms)
+
+verifier = Verifier(crs)
+print verifier.verify(ciphertexts, proof)
+
+TABLES = make_tables(crs.pk1, crs.pk2, crs.n)
+shuffled_ms = decrypt_messages(crs.gamma, TABLES, proof['shuffled_ciphertexts'])
+
+end = datetime.datetime.now()
+print "ellapsed: %s" % (end - start)
